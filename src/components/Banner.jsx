9df7044b1d544da3aa88f87c23c12a2473c4c974
Siddhesh Palimkar <57897149+Siddhesh30@users.noreply.github.com>
2020-08-30 22:59:16 +0530

    Add files via upload

---
+++ src/components/Banner.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Banner = () => {
+    return(
+        <div className="container">
+        <header style={{marginTop: '70px'}}>
+         <div className="headerText">
+         <div>
+         <h1>Order Now</h1>
+         </div>
+         </div>
+        </header>
+        </div>
+    )
+}
+
+export default Banner;
